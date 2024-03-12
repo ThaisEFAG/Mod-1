80cb43cf9ad8ff41325d6991c0f39b2f746b95b4
Thaís Gonçalves <thaisefag1992@gmail.com>
2024-03-11 23:57:04 -0300

    soma-carrinho

---
+++ 11-03/Nova pasta/script.js
@@ -0,0 +1,91 @@
+var cesta = doument.querySelector('.to-buy');
+let items = [
+    {
+        id: 1,
+        product: "arroz",
+        price: 25.00,
+        quantidade:0
+    },
+    {
+        id: 2,
+        product: feijão,
+        price: 16.00,
+        quantidade:0
+    },
+    {
+        id: 3,
+        product: açucar,
+        price: 20.00,
+        quantidade: 0
+    },
+    {
+        id: 4,
+        product: macarrão,
+        price: 7.00,
+        quantidade: 0
+    },
+    {
+        id: 5,
+        product: sal,
+        price: 4.00,
+        quantidade: 0
+    },
+    {
+        id: 6,
+        product: leite,
+        price: 8.00,
+        quantidade: 0
+    }
+]
+
+
+
+
+inicializarLoja = () => {
+    var containerProdutos = document.querySelector()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ao clicar no botão de incluir os ítens
+// let include = document.querySelector('.to-include');
+// let shopList = document.querySelector('.shop-list');
+// let dinamicUl = document.createElement('ul');
+
+// include.addEventListener("click", () => {
+//     productOne.forEach(item => {
+//         let li = document.createElement('li');
+//         li.textContent = item;
+//         dinamicUl.appendChild('li');
+//     });
+//     shopList.appendChild(dinamicUl);
+// })
+
+// // ao clicar no botão de pagamento
+// let payment = document.querySelector('.to-buy');
+
+// payment.addEventListener("click", () => {
+//     function soma(...valores){
+//         return valores.length
+//     }
+//     alert('O valor total é ${total}. Oriente o cliente para pagamento!')
+// })
+
---
+++ 11-03/soma-carrinho/script.js
@@ -0,0 +1,36 @@
+
+let select = document.getElementById("choice-product");
+let array = [];
+
+function addCar(){
+    
+    let optionC = select.options[select.selectedIndex].value;
+    
+    array.push(Number(optionC));
+    
+    console.log(array);
+
+}
+
+let showItens = document.querySelector('.show-Itens')
+
+select.addEventListener('change', function(){
+    var index = select.selectedIndex;
+    var textoIndex = select.options[index].text;
+    var para = document.createElement('p');
+    para.textContent = textoIndex;
+    showItens.appendChild(para);
+    
+})
+
+let precoTotal = document.querySelector('#total-preco');
+let total = 0;
+
+function somaCar (){
+    for(let i = 0; i < array.length; i++){
+        total += array[i];
+    }
+    precoTotal.textContent = total;
+    array.length = 0;
+}
+
